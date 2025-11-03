b21420cc256e0f7ffeb8b6d93756f8198ddbe1f2
JamesXiaoMo <jameswuxiaomo@gmail.com>
2025-11-03 15:45:29 +0900

    Refactor `HeatMapCreator` callbacks and clean up raw map attributes
    - Removed unused raw map boundary attributes.
    - Updated `callback` method to streamline parameter handling.
    - Enhanced `raw_grid_map_full_update` and `raw_grid_map_part_update` with improved initialization and transformation logic.

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from src.HeatMapCreator import HeatMapCreator
 
 
 if __name__ == '__main__':
-    test_list_1 = []
+    test_list_1 = [1, 0, 0, 1, 1, 0, 0, 0, 0]
     Hpc = HeatMapCreator()
-    Hpc.update(test_list, 5, 2, 1.0, 2)
+    Hpc.callback(test_list_1,
+                 3,
+                 3,
+                 1.0,
+                 2,
+                 2,)
--- src/HeatMapCreator.py
+++ src/HeatMapCreator.py
@@ -14,10 +14,6 @@ class HeatMapCreator:
         self.raw_grid_map_data = []               # 原始地图的数据
         self.raw_grid_map_origin_x = 0.0          # 原始地图原点x轴坐标 [pixel]
         self.raw_grid_map_origin_y = 0.0          # 原始地图原点y轴坐标 [pixel]
-        self.raw_grid_map_delta_x_positive = 0    # 原始地图数据x轴正方向到原点距离 [pixel]
-        self.raw_grid_map_delta_x_negative = 0    # 原始地图数据x轴负方向到原点距离 [pixel]
-        self.raw_grid_map_delta_y_positive = 0    # 原始地图数据y轴正方向到原点距离 [pixel]
-        self.raw_grid_map_delta_y_negative = 0    # 原始地图数据y轴负方向到原点距离 [pixel]
 
         self.raw_grid_map_data_2d = []            # 原始地图的2D数据
 
@@ -38,7 +34,6 @@ class HeatMapCreator:
         raw_grid_map_resolution: float,
         raw_grid_map_origin_x: float,
         raw_grid_map_origin_y: float,
-        heat_map_resolution: float,
     ):
         """
         用于在订阅ROS2的/map时的回调函数
@@ -48,20 +43,20 @@ class HeatMapCreator:
         :param raw_grid_map_resolution: 原始地图的分辨率 [m/pixel]
         :param raw_grid_map_origin_x: 原始地图原点x轴坐标 [pixel]
         :param raw_grid_map_origin_y: 原始地图原点y轴坐标 [pixel]
-        :param heat_map_resolution: 热力图的分辨率 [m/pixel]
         :return:
         """
-        self.raw_grid_map_width_pixel = raw_grid_map_width_pixel
-        self.raw_grid_map_height_pixel = raw_grid_map_height_pixel
-        self.raw_grid_map_resolution = raw_grid_map_resolution
-        self.raw_grid_map_width = raw_grid_map_width_pixel * raw_grid_map_resolution
-        self.raw_grid_map_height = raw_grid_map_height_pixel * raw_grid_map_resolution
-        self.raw_grid_map_data = raw_grid_map_data
 
         # 判断是否为第一次传入数据
         if self.counter == 0 and len(self.raw_grid_map_data_2d) == 0:
             # 第一次传入数据
-            self.raw_grid_map_full_update()
+            self.raw_grid_map_full_update(
+                raw_grid_map_data,
+                raw_grid_map_width_pixel,
+                raw_grid_map_height_pixel,
+                raw_grid_map_resolution,
+                raw_grid_map_origin_x,
+                raw_grid_map_origin_y,
+            )
         else:
             # 非第一次传入数据
             self.raw_grid_map_part_update()
@@ -75,35 +70,70 @@ class HeatMapCreator:
         # self.heat_map_width_pixel = self.heat_map_width // self.heat_map_resolution
         # self.heat_map_height_pixel = self.heat_map_height // self.heat_map_resolution
 
-    def raw_grid_map_full_update(self):
+    def raw_grid_map_full_update(
+            self,
+            raw_grid_map_data: list,
+            raw_grid_map_width_pixel: int,
+            raw_grid_map_height_pixel: int,
+            raw_grid_map_resolution: float,
+            raw_grid_map_origin_x: float,
+            raw_grid_map_origin_y: float,
+    ):
         """
         由原始地图数据完整转换为2D数据
         仅在第一次传入原始数据时调用
+        :param raw_grid_map_data: 原始地图的数据
+        :param raw_grid_map_width_pixel: 原始地图的宽度 [pixel]
+        :param raw_grid_map_height_pixel: 原始地图的高度 [pixel]
+        :param raw_grid_map_resolution: 原始地图的分辨率 [m/pixel]
+        :param raw_grid_map_origin_x: 原始地图原点x轴坐标 [pixel]
+        :param raw_grid_map_origin_y: 原始地图原点y轴坐标 [pixel]
         :return:
         """
-        # 创建原点到各边的距离
-        self.raw_grid_map_delta_x_positive = raw_grid_map_width_pixel - raw_grid_map_origin_x
-        self.raw_grid_map_delta_x_negative = raw_grid_map_origin_x
-        self.raw_grid_map_delta_y_positive = raw_grid_map_height_pixel - raw_grid_map_origin_y
-        self.raw_grid_map_delta_y_negative = raw_grid_map_origin_y
-        # 创建空数组
-        self.raw_grid_map_data_2d = np.full((self.raw_grid_map_height_pixel, self.raw_grid_map_width_pixel), -1)
-        # 传入原始地图的坐标原点（仅进行一次）
+        # 初始化
+        self.raw_grid_map_width_pixel = raw_grid_map_width_pixel
+        self.raw_grid_map_height_pixel = raw_grid_map_height_pixel
+        self.raw_grid_map_resolution = raw_grid_map_resolution
+        self.raw_grid_map_width = raw_grid_map_width_pixel * raw_grid_map_resolution
+        self.raw_grid_map_height = raw_grid_map_height_pixel * raw_grid_map_resolution
+        self.raw_grid_map_data = raw_grid_map_data
         self.raw_grid_map_origin_x = raw_grid_map_origin_x
         self.raw_grid_map_origin_y = raw_grid_map_origin_y
+        # 创建空数组
+        self.raw_grid_map_data_2d = np.full((self.raw_grid_map_height_pixel, self.raw_grid_map_width_pixel), -1)
         # 将原始地图的数据转化为2D形式(完整变换，仅进行一次)
         index = 0
-        for i in range(raw_grid_map_height_pixel):
-            for j in range(raw_grid_map_width_pixel):
+        for i in range(self.raw_grid_map_height_pixel):
+            for j in range(self.raw_grid_map_width_pixel):
                 self.raw_grid_map_data_2d[self.raw_grid_map_height_pixel - 1 - i][j] = self.raw_grid_map_data[index]
                 index += 1
         # debug: 检查转换是否正确
         print(f'原始地图2D数据:\n{self.raw_grid_map_data_2d}')
 
-    def raw_grid_map_part_update(self):
+    def raw_grid_map_part_update(
+            self,
+            raw_grid_map_data: list,
+            raw_grid_map_width_pixel: int,
+            raw_grid_map_height_pixel: int,
+            raw_grid_map_resolution: float,
+            raw_grid_map_origin_x: float,
+            raw_grid_map_origin_y: float,
+    ):
         """
         由原始地图数据新增部分加入现有的2D数据
         除了第一次以外传入原始数据时调用
+        :param raw_grid_map_data: 原始地图的数据
+        :param raw_grid_map_width_pixel: 原始地图的宽度 [pixel]
+        :param raw_grid_map_height_pixel: 原始地图的高度 [pixel]
+        :param raw_grid_map_resolution: 原始地图的分辨率 [m/pixel]
+        :param raw_grid_map_origin_x: 原始地图原点x轴坐标 [pixel]
+        :param raw_grid_map_origin_y: 原始地图原点y轴坐标 [pixel]
         :return:
         """
+        # 计算地图扩展的距离
+        delta_x_negative = self.raw_grid_map_origin_x - raw_grid_map_origin_x   # x轴负方向扩展距离
+        delta_y_negative = self.raw_grid_map_origin_y - raw_grid_map_origin_y   # y轴负方向扩展距离
+        delta_x_positive = raw_grid_map_width_pixel - self.raw_grid_map_width_pixel - delta_x_negative  # x轴正方向扩展距离
+        delta_y_positive = raw_grid_map_height_pixel - self.raw_grid_map_height_pixel - delta_y_negative    # y轴正方向扩展距离
+
 

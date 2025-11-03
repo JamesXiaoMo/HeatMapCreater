4f6305980deaf8b36f34972e8a9208fe4a78bb6b
JamesXiaoMo <jameswuxiaomo@gmail.com>
2025-11-03 22:18:57 +0900

    Backup.

--- main.py
+++ main.py
@@ -2,20 +2,11 @@ from src.HeatMapCreator import HeatMapCreator
 
 
 if __name__ == '__main__':
-    test_list_1_data = [1, 0, 0, 1, 1, 0, 0, 0, 0]
-    test_list_2_data = [1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0]
-    Hpc = HeatMapCreator()
+    test_list_1_data = [0] * 25
+    Hpc = HeatMapCreator(heat_map_interval=2.0)
     Hpc.callback(test_list_1_data,
-                 3,
-                 3,
+                 5,
+                 5,
                  1.0,
-                 2,
-                 2,)
-    print(f'{Hpc.raw_grid_map_width_pixel, Hpc.raw_grid_map_height_pixel}')
-    Hpc.callback(test_list_2_data,
-                 3,
-                 4,
-                 1.0,
-                 2,
-                 2)
-    print(f'{Hpc.raw_grid_map_width_pixel, Hpc.raw_grid_map_height_pixel}')
+                 -2.0,
+                 -2.0)
--- src/HeatMapCreator.py
+++ src/HeatMapCreator.py
@@ -5,26 +5,27 @@ class HeatMapCreator:
     """
     由ROS2发布的/map话题数据生成热力图
     """
-    def __init__(self):
-        self.raw_grid_map_width_pixel = 0         # 原始地图的宽度 [pixel]
-        self.raw_grid_map_height_pixel = 0        # 原始地图的高度 [pixel]
-        self.raw_grid_map_width = 0               # 原始地图的宽度 [m]
-        self.raw_grid_map_height = 0              # 原始地图的高度 [m]
-        self.raw_grid_map_resolution = 0          # 原始地图的分辨率 [m/pixel]
-        self.raw_grid_map_data = []               # 原始地图的数据
-        self.raw_grid_map_origin_x = 0.0          # 原始地图原点x轴坐标 [pixel]
-        self.raw_grid_map_origin_y = 0.0          # 原始地图原点y轴坐标 [pixel]
+    def __init__(self, heat_map_interval: float):
+        self.raw_grid_map_width_pixel = 0           # 原始地图的宽度 [pixel]
+        self.raw_grid_map_height_pixel = 0          # 原始地图的高度 [pixel]
+        self.raw_grid_map_width = 0                 # 原始地图的宽度 [m]
+        self.raw_grid_map_height = 0                # 原始地图的高度 [m]
+        self.raw_grid_map_resolution = 0            # 原始地图的分辨率 [m/pixel]
+        self.raw_grid_map_data = []                 # 原始地图的数据
+        self.raw_grid_map_origin_x = 0.0            # 原始地图原点x轴坐标 [pixel]
+        self.raw_grid_map_origin_y = 0.0            # 原始地图原点y轴坐标 [pixel]
 
-        self.raw_grid_map_data_2d = []            # 原始地图的2D数据
+        self.raw_grid_map_data_2d = []              # 原始地图的2D数据
 
-        self.heat_map_width_pixel = 0             # 热力图的宽度 [pixel]
-        self.heat_map_height_pixel = 0            # 热力图的高度 [pixel]
-        self.heat_map_width = 0                   # 热力图的宽度 [m]
-        self.heat_map_height = 0                  # 热力图的高度 [m]
-        self.heat_map_resolution = 0              # 热力图的分辨率 [m/pixel]
-        self.heat_map_data_2d = []                # 热力图的2D数据
+        self.heat_map_width_pixel = 0               # 热力图的宽度 [pixel]
+        self.heat_map_height_pixel = 0              # 热力图的高度 [pixel]
+        self.heat_map_width = 0                     # 热力图的宽度 [m]
+        self.heat_map_height = 0                    # 热力图的高度 [m]
+        self.heat_map_resolution = 0                # 热力图的分辨率 [m/pixel]
+        self.heat_map_interval = heat_map_interval  # 热力图的测量间隔 [m]
+        self.heat_map_data_2d = []                  # 热力图的2D数据
 
-        self.counter = 0                          # 获取原始数据的计数器
+        self.counter = 0                            # 获取原始数据的计数器
 
     def callback(
         self,
@@ -56,12 +57,23 @@ class HeatMapCreator:
         self.raw_grid_map_origin_y = raw_grid_map_origin_y
 
         # 创建空数组
-        self.raw_grid_map_data_2d = np.full((self.raw_grid_map_height_pixel, self.raw_grid_map_width_pixel), -1)
+        self.raw_grid_map_data_2d = np.full((2, self.raw_grid_map_height_pixel, self.raw_grid_map_width_pixel), -1)
         # 将原始地图的数据转化为2D形式(完整变换，仅进行一次)
         index = 0
         for i in range(self.raw_grid_map_height_pixel):
             for j in range(self.raw_grid_map_width_pixel):
-                self.raw_grid_map_data_2d[self.raw_grid_map_height_pixel - 1 - i][j] = self.raw_grid_map_data[index]
+                self.raw_grid_map_data_2d[0][self.raw_grid_map_height_pixel - 1 - i][j] = self.raw_grid_map_data[index]
                 index += 1
         # debug: 检查转换是否正确
         print(f'原始地图2D数据:\n{self.raw_grid_map_data_2d}')
+
+        # 绝对值原点坐标
+        true_origin_point = (abs(self.raw_grid_map_origin_x), self.raw_grid_map_height_pixel - abs(self.raw_grid_map_origin_y))
+        explore_origin_points = []
+        explore_results = []
+
+        print(true_origin_point)
+
+
+
+
